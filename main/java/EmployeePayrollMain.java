2afba348c79f45ed4ae5969c70fd1d5fd1cade5d
prajwalgunjal <prajwalgunjal8@gmail.com>
2023-04-18 14:08:09 +0530

    UC2 added

--- main/java/EmployeePayrollMain.java
+++ main/java/EmployeePayrollMain.java
@@ -1,37 +1,48 @@
-import java.sql.Connection;
-import java.sql.Driver;
-import java.sql.DriverManager;
+import java.sql.*;
 import java.util.Enumeration;
+
 public class EmployeePayrollMain {
     public static void main(String[] args) {
         String url
                 = "jdbc:mysql://localhost:3306/payroll_service";
         String username = "root";
-        String password = "Romil@33";
+        String password = "root";
         Connection con;
+        String query
+                = "select * from employee_payroll";
         try {
             Class.forName("com.mysql.cj.jdbc.Driver"); // Driver name
             System.out.println("Driver Loaded !!!");
-        }catch (ClassNotFoundException e) {
-            throw new IllegalStateException("Cannot find driver in class path ", e);
-        }
-
-        try{
             System.out.println("Connecting to database" + url);
             con = DriverManager.getConnection(url, username, password);
             System.out.println("Connection Established successfully");
-        } catch (Exception e){
-            e.printStackTrace();
-        }
+            Statement st = con.createStatement();
+            boolean result = st.execute(query); // Execute query
+            ResultSet rs = st.getResultSet();
+            if(result) {
+                while (rs.next()) {
+                    int id = rs.getInt("id"); // Retrieve name from db
+                    System.out.println(id); // Print result on console
 
-        listDrivers();
-    }
+                    String name = rs.getString("name"); // Retrieve name from db
+                    System.out.println(name); // Print result on console
 
-    private static void listDrivers() {
-        Enumeration<Driver> driverList = DriverManager.getDrivers();
-        while(driverList.hasMoreElements()){
-            Driver driverClass = (Driver) driverList.nextElement();
-            System.out.println(" " + driverClass.getClass().getName());
+                    String salary
+                            = rs.getString("salary"); // Retrieve name from db
+                    System.out.println(salary); // Print result on console
+
+                    String startDate
+                            = rs.getString("start"); // Retrieve name from db
+                    System.out.println(startDate); // Print result on console
+                }
+            } else {
+                System.out.println("Invalid Query");
+            }
+            st.close(); // close statement
+            con.close(); // close connection
+            System.out.println("Connection Closed....");
+        } catch (SQLException | ClassNotFoundException e){
+            System.out.println("Catch block");
         }
     }
 }
